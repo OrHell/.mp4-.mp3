2aae6cfca42b6b9bf604bbade829d1063fdb9550
OrHell <43348516+OrHell@users.noreply.github.com>
2019-07-02 19:39:23 +0300

    Add files via upload

--- convert.py
+++ convert.py
@@ -1,4 +1,5 @@
 import moviepy.editor as mp
+import shutil
 import os
 print("FUCK OFF SPACE")
 music = []
@@ -7,13 +8,28 @@ for folder, subfolders, files in os.walk(folder):
 	for file in files:
 		if file.endswith(".mp4"):
 			music.append(file)
+
 iterat = 0
 count = len(music)
-print(music)
+music_format = []
+for iterat in range(0,count):
+	music_format.append(music[iterat])
+
 for iterat in range(0, count):
-	music[iterat]=music[iterat][:-4]
-print(music)
+	music_format[iterat].replace(' ', '_')
+
+for iterat in range(0, count):
+	os.rename(music[iterat], music_format[iterat])
+
+
+for iterat in range(0, count):
+	music[iterat]=music_format[iterat][:-4]
+
 
 for iterat in range(0, count):
 	clip = mp.VideoFileClip(music[iterat]+".mp4")
 	clip.audio.write_audiofile(music[iterat]+".mp3")
+'''
+				os.rename(file_name, list_list[i]+'.png')
+				shutil.move(list_list[i]+'.png', "C:\\info\\order")
+				'''
